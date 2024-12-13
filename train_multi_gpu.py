dc58537b911584d897badc1cab52cfe16c0e3f59
Loc La Hoang <lhloc249@srl-login1.cm.cluster>
2024-12-13 10:00:48 +0100

    update training script

--- train_multi_gpu.py
+++ train_multi_gpu.py
@@ -19,7 +19,7 @@ def get_dataset(dataset_path="/home/lhloc249/D1/llm_dataset/pile-uncopyrighted/"
         dataset_path = dataset_path + f"{split}.jsonl.zst"
     dataset = load_dataset('json', data_files={split:dataset_path}, split=split, streaming=True)
     if shuffle:
-        return dataset.shuffle(buffer_size=10000, seed=42)
+        return dataset.shuffle(buffer_size=10_000, seed=42)
     return dataset
 
 def get_tokenizer():
@@ -33,7 +33,7 @@ config = Mamba2Config(
         vocab_size=50277,
         hidden_size=768,
         state_size=128,
-        num_hidden_layers=24,
+        num_hidden_layers=8,
         layer_norm_epsilon=1e-5,
         pad_token_id=1,
         bos_token_id=0,
@@ -59,39 +59,45 @@ config = Mamba2Config(
 ) 
 model = AutoModelForCausalLM.from_config(config)  #AutoModelForCausalLM.from_pretrained("state-spaces/mamba-130m-hf", use_cache=False, device_map={'':device_string})
 
+model_name = "mamba2_" + str(int(model.num_parameters())/1000000) + "M"
+print("Model Name", model_name)
 print("Number of Parameters:", model.num_parameters())
 model.to('cuda')
-train_dataset = get_dataset(split="train", shuffle=True) 
-valid_dataset = get_dataset(split="val", shuffle=True)
-
+train_dataset = get_dataset(split="train", shuffle=False) 
+valid_dataset = get_dataset(split="val", shuffle=False)
+#train_dataset = train_dataset.shard(num_shards=16, index=0)
+#valid_dataset = valid_dataset.shard(num_shards=16, index=0)
+#train_dataset = train_dataset.shuffle(buffer_size=10_000, seed=42)
+#valid_dataset = valid_dataset.shuffle(buffer_size=10_000, seed=42)
 #train_dataset = train_dataset.skip(1000)
 #valid_dataset = valid_dataset.take(1000)
 #print("Rank: ", int(os.environ["RANK"]), "WORLD_SIZE", int(os.environ["WORLD_SIZE"]))
-#train_dataset = split_dataset_by_node(train_dataset, rank=int(os.environ["RANK"]), world_size=int(os.environ["WORLD_SIZE"]))
-#valid_dataset = split_dataset_by_node(valid_dataset, rank=int(os.environ["RANK"]), world_size=int(os.environ["WORLD_SIZE"]))
+train_dataset = split_dataset_by_node(train_dataset, rank=int(os.environ["RANK"]), world_size=int(os.environ["WORLD_SIZE"]))
+valid_dataset = split_dataset_by_node(valid_dataset, rank=int(os.environ["RANK"]), world_size=int(os.environ["WORLD_SIZE"]))
 
 output_dir = './mamba_ckpt'
 tokenizer = get_tokenizer()
 tokenizer.pad_token = tokenizer.eos_token
 tokenizer.padding_side = "left" #enforce padding side left
 gradient_checkpointing_kwargs={'use_reentrant':False} 
-bs = 72
+bs = 24
+#num_gpus = 2
 training_args = SFTConfig(
-    output_dir="./results",
+    output_dir=f"./results/{model_name}",
     num_train_epochs=3,
     per_device_train_batch_size=bs,
     logging_dir='./logs',
     logging_steps=10,
     learning_rate=6e-4,
-    max_steps=int(4800*512/bs)+1, # 4800
+    max_steps=int(4800*512/bs/int(os.environ["WORLD_SIZE"])), # 4800
     max_seq_length=1024,
     fp16=True,
     #fp16=True,
-#    ddp_find_unused_parameters=True,
+    ddp_find_unused_parameters=True,
 #    torch_compile=True,
     gradient_checkpointing=True,
     gradient_checkpointing_kwargs=gradient_checkpointing_kwargs,
- #   gradient_accumulation_steps=2,
+    gradient_accumulation_steps=2,
     evaluation_strategy="steps",    # Evaluate during training
     eval_steps=500,                 # Perform evaluation every 500 steps
     save_strategy="steps",          # Save checkpoints during training
@@ -99,7 +105,7 @@ training_args = SFTConfig(
     save_total_limit=3,             # Keep only 3 checkpoints
     warmup_steps=500,
     report_to="tensorboard",        # Log training progress to TensorBoard
-    dataloader_num_workers=1,
+    dataloader_num_workers=0,
     packing=True
  )
 lora_config =  LoraConfig(
